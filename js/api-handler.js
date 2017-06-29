1ace9382d968a6c0b6f7272cdc334c0d0b6c1390
James Rock <hello@jamesrock.me>
2017-06-29 08:21:17 +0100

    updates

--- js/api-handler.js
+++ js/api-handler.js
@@ -1,16 +1,34 @@
 (function() {
 
-	apiHandler = function(apiName, options, sync) {
+	var
+	contentTypes = {
+		json: {
+			header: 'application/json',
+			parseIn: function(input) {
+				return JSON.parse(input);
+			},
+			parseOut: function(input) {
+				return JSON.stringify(input);
+			}
+		}
+	};
+
+	var
+	APIHandler = function(domain) {
+
+		this.domain = domain;
+		this.apis = {};
+
+	};
+	APIHandler.prototype.call = function(apiName, options, sync) {
 
 		var
 		xhr = new XMLHttpRequest(),
-		api = apis[apiName],
+		api = this.apis[apiName],
 		url = api.url,
 		contentType = contentTypes.json,
 		callback = 'error',
 		key;
-
-		// replace {domain} in url
 
 		if(options.params) {
 
@@ -57,5 +75,41 @@
 		return xhr;
 
 	};
+
+	var
+	APIHandlerService = function(apiHandler, name, method, url) {
+
+		this.name = name;
+		this.method = method;
+		this.url = url;
+
+		apiHandler.services[name] = this;
+
+	};
+
+	var
+	tumblrBlogAPI = new APIHandler('http://api.tumblr.com/v2/blog'),
+	tumblrOAuthAPI = new APIHandler('https://www.tumblr.com/oauth');
+
+	new APIHandlerService(tumblrAPI, 'getInfo', 'get', '/{blog-identifier}/info?api_key={key}');
+	new APIHandlerService(tumblrAPI, 'getPosts', 'get', '/{blog-identifier}/posts[/type]?api_key={key}&[optional-params=]');
+	new APIHandlerService(tumblrAPI, 'createPost', 'post', '/{blog-identifier}/post');
+	new APIHandlerService(tumblrAPI, 'editPost', 'post', '/{blog-identifier}/post/edit');
+
+	new APIHandlerService(tumblrOAuthAPI, 'requestToken', 'post', '/request_token');
+	new APIHandlerService(tumblrOAuthAPI, 'authorise', 'post', '/authorize');
+	new APIHandlerService(tumblrOAuthAPI, 'accessToken', 'post', '/access_token');
+
+	tumblrAPI.call('getPosts', {
+		success: function(response) {
+
+			console.log(response);
+
+		},
+		params: {
+			'blog-identifier': 'bob',
+			'key': 'key'
+		}
+	});
 
 })();
--- js/domains.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-	domains = {
-		twitter: '',
-		facebook: '',
-		instagram: ''
-	};
-
-})();
